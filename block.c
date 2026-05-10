5164dc011337afc2d6ca8635997a548cc0418f77
Ahmad Abdul Rehman <ahmadabdulrehman@Ahmads-Mac-mini.local>
2026-05-10 23:11:58 +0500

    Added memory cleanup in block manager

--- block.c
+++ block.c
@@ -61,6 +61,15 @@ BlockManager *divide_into_blocks(const char *filename, size_t block_size)
             this_block_size = block_size;
 
         manager->blocks[i].data = malloc(this_block_size);
+        if (!manager->blocks[i].data) {
+            for (int j = 0; j < i; j++) {
+                free(manager->blocks[j].data);
+            }
+            free(manager->blocks);
+            free(manager);
+            fclose(f);
+            return NULL;
+        }
 
         size_t bytes_read = fread(manager->blocks[i].data, 1, this_block_size, f);
         manager->blocks[i].size          = bytes_read;
@@ -89,6 +98,11 @@ int reassemble_blocks(BlockManager *manager, const char *output_filename)
     for (int i = 0; i < manager->num_blocks; i++)
     {
         size_t written = fwrite(manager->blocks[i].data, 1, manager->blocks[i].size, f);
+        if (written != manager->blocks[i].size) {
+            perror("fwrite");
+            fclose(f);
+            return -1;
+        }
     }
 
     fclose(f);
